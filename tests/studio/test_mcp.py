9e8f3ce03b3dcbb3d79b30bea55ceeaac9391551
THE PAULI EFFECT <executiveusa@gmail.com>
2026-08-29 11:48:31 -0600

    test: require UGC factory MCP tool

--- tests/studio/test_mcp.py
+++ tests/studio/test_mcp.py
@@ -7,6 +7,7 @@ def test_mcp_exposes_agent_first_surface():
         "studio_status",
         "create_campaign_plan",
         "create_ugc_prompt",
+        "create_ugc_ad_factory_plan",
         "list_social_accounts",
         "schedule_social_drop",
     }.issubset(names)
